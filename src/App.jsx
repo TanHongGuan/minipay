e162a652b86f17884f2174b06d7d3c22a6a81e0c
Yuan <yuan291104@gmail.com>
2025-10-17 22:52:19 +0800

    Added icons for Top-Up, Transfer, Cash-Flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import profile from "./assets/Profile-Icon.png";
+import cashFlow from "./assets/Cash-Flow.png";
+import topUp from "./assets/Top-Up.png";
+import transfer from "./assets/Transfer.png";
+
 export default function App() {
   return (
     <div className="App-shell">
@@ -19,16 +24,23 @@ export default function App() {
             </div>
           </div>
 
+          {/* Settings */}
           <div className="Settings">
-
+            <img id="Profile-Icon" src={profile} alt="profile"/>
           </div>
+      </div>
 
+      <div id="Operations">
+        <img id="Top-Up" src={topUp} alt="top-up"/>
+        <img id="Transfer" src={transfer} alt="transfer"/>
+        <img id="Cash-Flow" src={cashFlow} alt="cash-flow"/>
 
       </div>
 
-
-
-
     </div>
   );
+}
+
+function bottomNavBar() {
+  
 }
